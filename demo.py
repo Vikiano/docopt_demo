6f4851edb9ae6a6a41f1c5b187d0cefdf0cb2043
Victor Francis <francisvictor47@gmail.com>
2021-11-18 18:31:25 -0800

    Add main() function to demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 
 Options:
-<arg>             Takes any value (this is a required positional argument)
+<arg1>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 [--arg4=<arg4>]   Takes any value (this is an optional option)
@@ -13,5 +13,14 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
+# print(opt)
+# print(type(opt))
+
+
+def main(arg1, arg2, arg3, arg4):
+    print(opt)
+    print(type(opt))
+
+
+if __name__ == "__main__":
+    main(opt["<arg1>"], opt["--arg2"], opt["--arg3"], opt["--arg4"])
